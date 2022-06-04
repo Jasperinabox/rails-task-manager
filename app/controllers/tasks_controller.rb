d7ec3ea8f60fe706426a9fdfb5cfd62caf6f1b6b
Jasperzhang <zed0416660907@gmail.com>
2022-06-04 16:37:34 +1000

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  def title
+    @tasks = Task.all
+  end
+
+  def details
+    @tasks = Task.find(params[:id])
+  end
+
+  def completed
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(tasks_params)
+    @tasks.save
+    redirect_to task_path(@tasks)
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(tasks_params)
+    redirect_to task_path(@tasks)
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
